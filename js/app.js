57e33f6c9b173e9307f9e07de18f38b1f569eed7
mtis <kachurvova@gmail.com>
2020-03-17 03:50:08 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,55 @@
+function renderTable(countries) {
+    let countriesToRender = countries.map(country => {
+        return {name, population, area, capital} = country;
+    });
+
+    let tableStr = `<table class="table table-bordered">
+        <thead>
+        <tr><td>Name</td><td>Capital</td><td>Population</td><td>Area</td></tr>
+        </thead>
+        <tbody>`;
+
+
+    for (let item of countriesToRender) {
+        tableStr += `<tr>
+            <td>${item.name}</td>
+            <td>${item.population}</td>
+            <td>${item.area}</td>
+            <td>${item.capital}</td>          
+        </tr>`;
+    }
+    $('.placeForTable').append(tableStr);
+
+
+}
+
+
+let getCountries = () => {
+
+    $('.btnGetCounties').attr('disabled', 'disabled');
+    $.ajax({
+            url: "https://restcountries.eu/rest/v2/all",
+
+            success: function (countries) {
+                console.log('ok');
+                renderTable(countries);
+                $('.btnGetCounties').removeAttr('disabled');
+            },
+            error: errorStr => {
+                console.log('error');
+                $('.btnGetCounties').removeAttr('disabled');
+            }
+        }
+    );
+}
+
+
+
+jQuery(document).ready(function ($) { //когда страница прогрузилась
+    $('.btnGetCounties').on('click ', e => {
+        console.log('ok');
+        getCountries();
+    });
+});
+
+
